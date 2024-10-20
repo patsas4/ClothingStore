728e6e31513bfe6e6c87613882015f2b11f85ba4
Petros Atsas <petrosatsas@gmail.com>
2024-10-20 19:48:50 -0400

    Main Controller and Twig for Home and Products pages

---
+++ src/Controller/MainController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Link;
+
+class MainController extends AbstractController
+{
+    public $links;
+    
+    public function __construct()
+    {
+        $this->links = [
+            new Link("/", "Home"),
+            new Link("/products","Products"),
+            new Link("/account","Account")
+        ];
+    }
+
+    public function filterLinks(string $name)
+    {
+        return array_filter($this->links, function (Link $link) use ($name) {
+            return $link->getName() != $name;
+        });
+    }
+
+    #[Route(path:"", name:"HomePage")]
+    public function HomePage() 
+    {
+        try
+        {
+            return $this->render("Main/home.html.twig", ["links" => $this->filterLinks("Home")], response: new Response());
+        }
+        catch(\Exception $e)
+        {
+            echo $e;
+            return new Response("", Response::HTTP_INTERNAL_SERVER_ERROR);
+        }        
+    }
+
+    #[Route(path:"/products", name:"Products")]
+    public function ProductsPage()
+    {
+        try
+        {
+            return $this->render("Main/products.html.twig", ["links" => $this->filterLinks("Products")], response: new Response());
+        }
+        catch(\Exception $e)
+        {
+            echo $e;
+            return new Response("", Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
